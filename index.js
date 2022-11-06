7865f8903f14073336fcce98603eb05ebd9625b9
vijaykumardumpeti <vijaydumpeti70@gmail.com>
2022-11-06 09:13:42 +0000

    Super Over League

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  //   Write your code here.
+
+  <div class="container">
+    <h1>Super Over League</h1>
+    <div className="image-container">
+      <img
+        className="image"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      />
+      <img
+        className="image"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
